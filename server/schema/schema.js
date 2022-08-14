a6447b8c08ba5fda4373e105af4db2ca4204f0f8
Karolin <karolin.thunman@gmail.com>
2022-08-14 18:05:21 +0200

    fix indentation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 
-const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull} = require("graphql")
+const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, GraphQLEnumType} = require("graphql")
 const Project = require("../models/Project")
 const Client = require("../models/Client")
 
@@ -63,32 +63,61 @@ const RootQuery = new GraphQLObjectType({
 
 const mutation = new GraphQLObjectType({
     name: "Mutation",
-    fields: {addClient:{
-        type: ClientType,
-        args: {
-            name: {type: GraphQLNonNull(GraphQLString)},
-            email: {type: GraphQLNonNull(GraphQLString)},
-            phone: {type: GraphQLNonNull(GraphQLString)},
+    fields: {
+        addClient:{
+            type: ClientType,
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)},
+                email: {type: GraphQLNonNull(GraphQLString)},
+                phone: {type: GraphQLNonNull(GraphQLString)},
+            },
+            resolve(parent,args){
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone
+                })
+                return client.save()
+            }
         },
-        resolve(parent,args){
-            const client = new Client({
-                name: args.name,
-                email: args.email,
-                phone: args.phone
-            })
-            return client.save()
-        }
-    },
-    deleteClient: {
-        type: ClientType,
-        args: {
-            id: {type: GraphQLNonNull(GraphQLID)} 
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: {type: GraphQLNonNull(GraphQLID)} 
+            },
+            resolve(parent,args){
+                return Client.findByIdAndRemove(args.id)
+            }
         },
-        resolve(parent,args){
-            return Client.findByIdAndRemove(args.id)
+        addProject: {
+            type: ProjectType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString)},
+                description: { type: GraphQLNonNull(GraphQLString)},
+                status: { 
+                    type: new GraphQLEnumType({
+                        name: "ProjectStatus",
+                        values: {
+                            "new": {value: "Not Started"},
+                            "progress": {value: "In Progress"},
+                            "completed": {value: "Completed"}
+                        }
+                    }),
+                    defaultValue: "Not started"
+                },
+                clientId: {type: GraphQLNonNull(GraphQLID)}
+            },
+            resolve(parent,args){
+                const project = new Project({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId
+                })
+                return project.save()
+            }
         }
     }
-}
 })
 
 module.exports = new GraphQLSchema({
